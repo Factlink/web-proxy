ab76d81d2b027c3f769e06310f5dc0e506c5fda3
Martijn Russchen <martijn@factlink.com>
2014-04-01 12:03:38 +0200

    Replace raven with appsignal

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ dokku config:set proxy-production redirect_for_no_url='https://factlink.com' \
   hostname='fct.li' \
   host='http://fct.li' \
   jslib_uri='https://static.factlink.com/lib/dist/factlink_loader.min.js?o=proxy' \
-  raven_dsn='...'
+  APPSIGNAL_PUSH_API_KEY='...'
 ```
 
 ## Contributing
---
+++ lib/appsignal_goliath_listener.rb
@@ -0,0 +1,18 @@
+require 'appsignal'
+
+Appsignal.config =
+  Appsignal::Config.new(ENV['PWD'], Goliath.env, name: "Factlink Proxy")
+Appsignal.start_logger(nil)
+Appsignal.start
+
+class AppsignalGoliathListener
+  include Goliath::Rack::AsyncMiddleware
+
+  def post_process(env, status, headers, body)
+    if Appsignal.active? && env['rack.exception']
+      Appsignal.send_exception(env['rack.exception'])
+    end
+
+    [status, headers, body]
+  end
+end
